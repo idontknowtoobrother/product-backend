d38d6b68e3a3d8529c47f576399ff9c0387e93ff
hex <idontknowbrotherpls@gmail.com>
2022-03-11 05:36:48 +0700

    added: Product.js type 'model', added: product.js type 'script for DB'

---
+++ dbscript/product.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+const Product = require("../models/Product")
+
+// connect Mongo
+mongoose.connect('mongodb://localhost:27017/example')
+
+
+const main = async function(){
+    
+}
---
+++ models/Product.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose') 
+const { Schema } = mongoose
+const productSchema = Schema({
+    name: String,
+    price: Number
+})
+
+module.exports = mongoose.model('Product', productSchema)
