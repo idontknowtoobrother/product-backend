cabc64ed3aa8961abf7e1cee0d52d192e48383f5
hex <idontknowbrotherpls@gmail.com>
2022-03-11 05:41:22 +0700

    added: 'main' that mockup data to mongodb

--- dbscript/product.js
+++ dbscript/product.js
@@ -4,7 +4,22 @@ const Product = require("../models/Product")
 // connect Mongo
 mongoose.connect('mongodb://localhost:27017/example')
 
-
-const main = async function(){
+async function clearProduct(){
     
 }
+
+
+// main
+async function main(){
+    for(var i=0; i < 12; i++){
+        const product = new Product({
+            name: `Product # ${i}`,
+            price: i*1000
+        }).save()
+        product.save()
+    }
+}
+
+main.then(()=>{
+    console.log('\'main\' done.');
+})
