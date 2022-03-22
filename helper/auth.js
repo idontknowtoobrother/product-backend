79dc1746d1e1ecf1dac40f831dd58feaa82a86d2
hex <idontknowbrotherpls@gmail.com>
2022-03-22 11:58:57 +0700

    forgot space

--- helper/auth.js
+++ helper/auth.js
@@ -6,9 +6,10 @@ const generateAccessToken = function(user) {
 
 const authMiddleware = function(req, res, next){
     const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split('')[1]
+    const token = authHeader && authHeader.split(' ')[1]
+    
     if(token == null) return res.sendStatus(401)
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, process.env.TOKEN_SECRET, function(err, user){
         console.log(err)
         if(err) return res.sendStatus(403)
         console.log(user)
